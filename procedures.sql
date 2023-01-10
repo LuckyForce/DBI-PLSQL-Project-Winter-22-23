16a88f4f59cea88e49058be3c5d0c8e4ddbaa34b
LuckyForce <adrian.schauer@aon.at>
2023-01-10 10:39:04 +0100

    Procedure planning

---
+++ procedures.sql
@@ -0,0 +1,24 @@
+-------------------
+-- Procedures
+-------------------
+
+-- Procedure register(email, password)
+
+-- Procedure set_personal_data(FirstName, LastName, Gender, BirthDate, user_id)
+
+-- Procudure delete_acc(user_id)
+
+-- Procedure create_club(name, link)
+
+-- Procedure create_event(name, description, date, time, location, club_id, user_id)
+
+-- Procedure create_news(title, description, date, club_id, user_id)
+
+-- Procedure create_court(name, location, club_id)
+
+-- Procedure reservate(court_id, date, time, user_id)
+
+-- Procedure edit_club_link(club_id, link)
+
+-- Procedure edit_club_name(club_id, name)
+
